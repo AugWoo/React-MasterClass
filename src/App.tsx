227d82238b72b11d4d87d7d8efb7464bff7a8fc3
AugWoo <wh000809@naver.com>
2022-12-18 14:16:16 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from './Circle';
+
+function App() {
+  return (
+    <div>
+      <Circle borderColor='black' bgColor='teal' text='teal Circle' />
+      <Circle bgColor='tomato' />
+    </div>
+  );
+}
+
+export default App;
