a4f7843cc1907c45a0d2977c21cb5612b12dcf57
AugWoo <wh000809@naver.com>
2022-12-23 15:02:07 +0900

    nested routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import App from './App';
 import { theme } from './theme';
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </QueryClientProvider>,
   document.getElementById('root')
 );
